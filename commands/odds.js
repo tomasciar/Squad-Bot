198707942317af5e7e4ade3863a4bb53c189f145
tomasciar <tomasciarlariello@gmail.com>
2022-08-09 23:28:11 -0400

    Add odds command

--- commands/odds.js
+++ commands/odds.js
@@ -3,11 +3,24 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('odds')
-    .setDescription('Repeats everything said after the command')
-    .addStringOption(option => option.setName('input').setDescription('Enter a sentence').setRequired(true)),
+    .setDescription('Plays a game of "What Are the Odds?"')
+    .addIntegerOption(option =>
+      option.setName('lower-limit').setDescription('Enter the lower-limit for the range').setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option.setName('upper-limit').setDescription('Enter the upper-limit for the range').setRequired(true)
+    )
+    .addUserOption(option => option.setName('user').setDescription('Enter the @ of a user').setRequired(true))
+    .addStringOption(option =>
+      option.setName('prompt').setDescription('Enter the scenario (ex. "eats a watermelon")').setRequired(true)
+    ),
 
   async execute(interaction) {
-    const string = interaction.options.getString('input');
-    await interaction.reply({ content: `${string}`, ephemeral: false });
+    const lowerLimit = interaction.options.getString('lower-limit');
+    const upperLimit = interaction.options.getString('upper-limit');
+    const user = interaction.options.getString('user');
+    const scenario = interaction.options.getString('scenario');
+
+    await interaction.reply({ content: `What are the odds that ${user} ${scenario}?`, ephemeral: false });
   }
 };
